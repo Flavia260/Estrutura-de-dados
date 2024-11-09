916610600735f60f17822583ff18f8d1b50dff12
flavia260 <flavialemos127@gmail.com>
2024-11-09 18:10:28 -0300

    heap

--- PilhaDinamica/EmpilhavelDinamica.kt
+++ PilhaDinamica/EmpilhavelDinamica.kt
@@ -1,4 +1,12 @@
 package PilhaDinamica
 
 interface EmpilhavelDinamica {
+    fun empilhar (dado: Any)
+    fun desempilhar (): Any?
+    fun estaCheia (): Boolean
+    fun estaVazia (): String
+    fun imprimir (): String
+    fun olhar (): Any?
+    fun atualizar (dado: Any)
+
 }
