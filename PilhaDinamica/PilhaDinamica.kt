7b08ae24bc017631dd3c8bb26e73446e0d95363b
flavia260 <flavialemos127@gmail.com>
2024-11-15 09:33:14 -0300

    heap

--- PilhaDinamica/PilhaDinamica.kt
+++ PilhaDinamica/PilhaDinamica.kt
@@ -1,24 +1,64 @@
 package PilhaDinamica
 
-class PilhaDinamica {
-    private var quantidade = 0
+class PilhaDinamica (private val tamanho : Int = 10) {
+    private var qnt = 0
     private var ponteiroTopo : NoDuplo ? = null
 
     fun empilhar (dado: Any){
         if (!estaCheia()){
             var noTemp : NoDuplo(dado)
-
+            noTemp.anterior = ponteiroTopo
+            if (!estaVazia())
+                ponteiroTopo?.proximo = noTemp
+            ponteiroTopo = noTemp
+            qnt = qnt++
+        }else{
+            println("A pilha está cheia")
         }
     }
-    fun desempilhar (): Any?
+    fun desempilhar (): Any? {
+        var dadotopo: Any? = null
+        if (!estaVazia()) {
+            var dadoTopo = ponteiroTopo?.dado
+            ponteiroTopo = ponteiroTopo?.anterior
+            qnt = qnt--
+            if (!estaCheia())
+                ponteiroTopo?.proximo = null
+        } else {
+            println("A pilha esta vazia")
+        }
+        return dadotopo
+    }
     fun estaCheia (): Boolean{
-        return quantidade == tamanho
+        return qnt == tamanho
     }
     fun estaVazia (): Boolean {
-        return quantidade == 0
+        return qnt == 0
     }
-    fun imprimir (): String
+    fun imprimir (): String{
+        var aux = ponteiroTopo
+        var resultado = "["
+        for (i in 0 until qnt){
+            resultado += "${aux?.dado}"
+            if (i != qnt - 1)
+                resultado += ","
+            aux = aux?.anterior
+        }
+        return "$resultado"
+    }
 
-    fun olhar (): Any?
-    fun atualizar (dado: Any)
+    fun olhar (): Any?{
+        var topo : Any? = null
+        if (!estaVazia())
+            topo = ponteiroTopo?.dado
+        else
+            println("A pilha está vazia")
+        return topo
+    }
+    fun atualizar (dado: Any){
+        if (!estaVazia())
+            ponteiroTopo?.dado = dado
+        else
+            println("A pilha está vazia")
+    }
 }
