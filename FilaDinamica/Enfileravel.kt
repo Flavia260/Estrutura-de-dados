c47cbb00f857e4466a5e6e450d9302967dee19fa
flavia260 <flavialemos127@gmail.com>
2024-11-23 16:50:41 -0300

    heap

--- FilaDinamica/Enfileravel.kt
+++ FilaDinamica/Enfileravel.kt
@@ -1,4 +1,12 @@
 package FilaDinamica
 
 interface Enfileravel {
+    fun enfileirar(dado: Any?)
+    fun desenfileirar(): Any?
+    fun atualizar(dado: Any?)
+    fun frente(): Any?
+    // Metodos auxiliares
+    fun estaCheia(): Boolean
+    fun estaVazia(): Boolean
+    fun imprimir(): String
 }
---
+++ FilaDinamica/FilaDinamica.kt
@@ -0,0 +1,64 @@
+package FilaDinamica
+
+class FilaDinamica (private val tamanho : Int): Enfileravel {
+    private var ponteiroInicio: NoDuplo? = null
+    private var ponteiroFim: NoDuplo? = null
+    private var quantidade = 0
+
+    override fun enfileirar(dado: Any?) {
+        if (!estaCheia()){
+            val novoNo = NoDuplo(dado)
+            ponteiroFim?.proximo = novoNo
+            novoNo.anterior = ponteiroFim
+            ponteiroFim = novoNo
+            quantidade++
+        }else{
+            throw NoSuchElementException ("Lista cheia")
+        }
+    }
+    override fun desenfileirar(): Any? {
+        var aux : Any? = null
+        if (!estaVazia()){
+            aux = ponteiroInicio?.dado
+            ponteiroInicio = ponteiroInicio?.proximo
+            if (ponteiroInicio != null)
+                ponteiroInicio?.anterior = null
+            else
+                ponteiroFim = null
+            quantidade--
+        }else {
+            throw NoSuchElementException ("Lista Vazia")
+        }
+        return aux
+    }
+    override fun atualizar(dado: Any?){
+        if (!estaVazia())
+            ponteiroInicio?.dado = dado
+        else
+            throw NoSuchElementException ("Lista Vazia")
+    }
+    override fun frente(): Any?{
+        var aux : Any? = null
+        if (!estaVazia())
+            aux = ponteiroInicio?.dado
+        else
+            throw NoSuchElementException ("Lista Vazia")
+        return aux
+    }
+    // Metodos auxiliares
+    override fun estaCheia(): Boolean{
+        return tamanho== quantidade
+    }
+    override fun estaVazia(): Boolean {
+        return quantidade==0
+    }
+    override fun imprimir(): String{
+        var retorno = "["
+        var aux = ponteiroInicio
+        for (i in 0 until quantidade){
+            retorno += aux?.dado
+            aux =
+        }
+        return aux
+    }
+}
